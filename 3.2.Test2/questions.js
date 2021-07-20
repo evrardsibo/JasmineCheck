855f1150d4d714f2c650fe187345f7ca36329d25
evrardsibo <evrard.sibomana@gmail.com>
2021-07-20 09:46:29 +0200

    jas2fini

--- 3.2.Test2/questions.js
+++ 3.2.Test2/questions.js
@@ -1,43 +1,76 @@
 let CreationTableauLangages =  () => {
+    let langages = ["Html", "CSS", "Java", "PHP"]
+    return langages
 
 }
 
 let CreationTableauNombres =  () => {
 
+    let nombres = [ 0, 1, 2, 3, 4, 5 ]
+    return nombres
+
 }
 
 let RemplacementElement =  (langages) => {
+    langages.splice(2,1,"Javascript")
+    return langages
 
 }
 
 let AjoutElementLangages =  (langages) => {
 
+    langages.push('Ruby', 'Python')
+    return langages
+
 }
 
 let AjoutElementNombres =  (nombres) => {
+
+ nombres.unshift(-2, -1)
+ return nombres
 
 }
 
 let SuppressionPremierElement =  (langages) => {
 
+    langages.shift()
+    return langages
+
+
 }
 
 let SuppressionDernierElement =  (langages) => {
+    langages.pop()
+    return langages
 
 }
 
 let ConversionChaineTableau =  (reseaux_sociaux_chaine) => {
 
+    let reseaux_sociaux = [ 'Facebook', 'Twitter', 'Google +', 'Viadeo', 'LinkedIn' ]
+
+    reseaux_sociaux == reseaux_sociaux_chaine.split('')
+    return reseaux_sociaux
+
+
 }
 
 let ConversionTableauChaine =  (langages) => {
+    
+     let conv = langages.join(',')
+    return conv
+
 
 }
 
 let TriTableau =  (reseaux_sociaux) => {
 
+    return reseaux_sociaux.sort()
+
 }
 
 let InversionTableau =  (langages) =>{
+
+    return langages.reverse()
 
 }
